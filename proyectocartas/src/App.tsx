6cbd8cde1c400014899e8e1eb4da0c921bd6217f
Sirxnine <monterolaleo23@gmail.com>
2025-11-29 01:50:33 -0400

    Estructura básica de las cartas y un estado por defecto
    Se creo el molde por defecto que va a llevar cada carta y se le dio un estado por defecto que va a llevar cada una

--- proyectocartas/src/App.tsx
+++ proyectocartas/src/App.tsx
@@ -1,14 +1,42 @@
 import { useState } from 'react'
+
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-     
-    </>
-  )
+interface ICarta {
+  id: number;
+  nombre: string;
+  imagen: string;
+  descripcion: string;
+  tipo: string;
+  rareza: string;
+  ataque: number;
+  defensa: number;
 }
 
-export default App
+const cartasIniciales: ICarta[] = [
+  {
+    id: 1,
+    nombre: "Duki",
+    imagen: "https://wallpapers.com/images/hd/duki-dragon-ball-cloud-rd5m0qun2l3u43ui.jpg",
+    descripcion: "El Rey creador del trono musical",
+    tipo: "Musico",
+    rareza: "Legendaria",
+    ataque: 300,
+    defensa: 300
+  },
+  {
+    id: 2,
+    nombre: "Milo J",
+    imagen: "https://images.genius.com/bc9018a99d67265c476dbaad883ba8d5.1000x1000x1.png", 
+    descripcion: "El Heredero al Trono, la cura del alma",
+    tipo: "Musico",
+    rareza: "Épica",
+    ataque: 200,
+    defensa: 200
+  }
+];
+const [cartas, setCartas] = useState<ICarta[]>(cartasIniciales);
+const [mostrarFormulario, setMostrarFormulario] = useState<boolean>(false);
+const [cartaEditando, setCartaEditando] = useState<ICarta | null>(null);
+
+export default cartasIniciales
---
+++ proyectocartas/src/componentes/Header.tsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header>
+      <h1>Gestor de Cartas</h1>
+    </header>
+  );
+}
+
+export default Header;
